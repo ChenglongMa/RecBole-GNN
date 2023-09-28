4585e47eb7919ea97bcf12f09b4459c751fe18ac
Chenglong Ma <s3629862@rschprdap02.csit.rmit.edu.au>
2023-09-28 23:06:20 +1000

    Update quick start

--- recbole_gnn/quick_start.py
+++ recbole_gnn/quick_start.py
@@ -6,8 +6,15 @@ from recbole_gnn.config import Config
 from recbole_gnn.utils import create_dataset, data_preparation, get_model, get_trainer
 
 
-def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict=None, saved=True, queue=None):
-    r""" A fast running api, which includes the complete process of
+def run_recbole_gnn(
+    model=None,
+    dataset=None,
+    config_file_list=None,
+    config_dict=None,
+    saved=True,
+    queue=None,
+):
+    r"""A fast running api, which includes the complete process of
     training and testing a model on a specified dataset
     Args:
         model (str, optional): Model name. Defaults to ``None``.
@@ -18,8 +25,13 @@ def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict
         queue (torch.multiprocessing.Queue, optional): The queue used to pass the result to the main process. Defaults to ``None``.
     """
     # configurations initialization
-    config = Config(model=model, dataset=dataset, config_file_list=config_file_list, config_dict=config_dict)
-    init_seed(config['seed'], config['reproducibility'])
+    config = Config(
+        model=model,
+        dataset=dataset,
+        config_file_list=config_file_list,
+        config_dict=config_dict,
+    )
+    init_seed(config["seed"], config["reproducibility"])
     # logger initialization
     init_logger(config)
     logger = getLogger()
@@ -34,29 +46,33 @@ def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict
     train_data, valid_data, test_data = data_preparation(config, dataset)
 
     # model loading and initialization
-    init_seed(config['seed'], config['reproducibility'])
-    model = get_model(config['model'])(config, train_data.dataset).to(config['device'])
+    init_seed(config["seed"], config["reproducibility"])
+    model = get_model(config["model"])(config, train_data.dataset).to(config["device"])
     logger.info(model)
 
     # trainer loading and initialization
-    trainer = get_trainer(config['MODEL_TYPE'], config['model'])(config, model)
+    trainer = get_trainer(config["MODEL_TYPE"], config["model"])(config, model)
 
     # model training
     best_valid_score, best_valid_result = trainer.fit(
-        train_data, valid_data, saved=saved, show_progress=config['show_progress']
+        train_data, valid_data, saved=saved, show_progress=config["show_progress"]
     )
 
     # model evaluation
-    test_result = trainer.evaluate(test_data, load_best_model=saved, show_progress=config['show_progress'])
+    test_result = trainer.evaluate(
+        test_data, load_best_model=saved, show_progress=config["show_progress"]
+    )
+    topk_results = test_result.pop("topk_results")
 
-    logger.info(set_color('best valid ', 'yellow') + f': {best_valid_result}')
-    logger.info(set_color('test result', 'yellow') + f': {test_result}')
+    logger.info(set_color("best valid ", "yellow") + f": {best_valid_result}")
+    logger.info(set_color("test result", "yellow") + f": {test_result}")
 
     result = {
-        'best_valid_score': best_valid_score,
-        'valid_score_bigger': config['valid_metric_bigger'],
-        'best_valid_result': best_valid_result,
-        'test_result': test_result
+        "best_valid_score": best_valid_score,
+        "valid_score_bigger": config["valid_metric_bigger"],
+        "best_valid_result": best_valid_result,
+        "test_result": test_result,
+        "topk_results": topk_results,
     }
 
     if config["local_rank"] == 0 and queue is not None:
@@ -66,7 +82,7 @@ def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict
 
 
 def objective_function(config_dict=None, config_file_list=None, saved=True):
-    r""" The default objective_function used in HyperTuning
+    r"""The default objective_function used in HyperTuning
 
     Args:
         config_dict (dict, optional): Parameters dictionary used to modify experiment parameters. Defaults to ``None``.
@@ -75,19 +91,21 @@ def objective_function(config_dict=None, config_file_list=None, saved=True):
     """
 
     config = Config(config_dict=config_dict, config_file_list=config_file_list)
-    init_seed(config['seed'], config['reproducibility'])
+    init_seed(config["seed"], config["reproducibility"])
     logging.basicConfig(level=logging.ERROR)
     dataset = create_dataset(config)
     train_data, valid_data, test_data = data_preparation(config, dataset)
-    init_seed(config['seed'], config['reproducibility'])
-    model = get_model(config['model'])(config, train_data.dataset).to(config['device'])
-    trainer = get_trainer(config['MODEL_TYPE'], config['model'])(config, model)
-    best_valid_score, best_valid_result = trainer.fit(train_data, valid_data, verbose=False, saved=saved)
+    init_seed(config["seed"], config["reproducibility"])
+    model = get_model(config["model"])(config, train_data.dataset).to(config["device"])
+    trainer = get_trainer(config["MODEL_TYPE"], config["model"])(config, model)
+    best_valid_score, best_valid_result = trainer.fit(
+        train_data, valid_data, verbose=False, saved=saved
+    )
     test_result = trainer.evaluate(test_data, load_best_model=saved)
 
     return {
-        'best_valid_score': best_valid_score,
-        'valid_score_bigger': config['valid_metric_bigger'],
-        'best_valid_result': best_valid_result,
-        'test_result': test_result
+        "best_valid_score": best_valid_score,
+        "valid_score_bigger": config["valid_metric_bigger"],
+        "best_valid_result": best_valid_result,
+        "test_result": test_result,
     }
