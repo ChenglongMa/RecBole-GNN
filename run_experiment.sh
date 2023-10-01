45e9bae120bd6479c76444f7d97a17e7723be698
Chenglong Ma <s3629862@rschprdap02.csit.rmit.edu.au>
2023-10-01 16:17:55 +1100

    Update code

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,14 +1,6 @@
 #! /bin/bash
 
 mine_dir=/home/sh2/S3629862/projects/mine
-for proj_dir in RecBole RecBole-GNN
-do
-  for dir_name in log log_tensorboard saved
-  do
-    dir2del="$mine_dir/$proj_dir/$dir_name"
-    echo "Deleting $dir2del"
-    rm -rf "${dir2del:?}/*"
-  done
-done
+rm -rv $mine_dir/{RecBole,RecBole-GNN}/{log,log_tensorboard,saved}/*
 
-python /home/sh2/S3629862/projects/mine/RecBole-GNN/run_recbole_gnn_group.py -m GRU4Rec,SASRec,LESSR,NISER,TAGNN,LightGCN,NGCF,GCSAN,GCEGNN -d ml-1m --config_files ./recbole_gnn/properties/overall.server.yaml --nproc 1
+python $mine_dir/RecBole-GNN/run_recbole_gnn_group.py -m GRU4Rec,SASRec,LESSR,NISER,TAGNN,LightGCN,NGCF,GCSAN,GCEGNN -d ml-1m --config_files ./recbole_gnn/properties/overall.server.yaml --nproc 1
