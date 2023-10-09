ff5f2b6b5a81da83a99af7166081121c1d7e7841
Chenglong Ma <s3629862@rschprdap02.csit.rmit.edu.au>
2023-10-09 13:26:01 +1100

    Update experiments

--- multi_run_experiment.sh
+++ multi_run_experiment.sh
@@ -2,8 +2,8 @@
 mine_dir=/home/sh2/S3629862/projects/mine
 
 
-# SASRec
-for model in LESSR NISER TAGNN LightGCN NGCF GCSAN GCEGNN GRU4Rec
+# SASRec LESSR Ours NISER TAGNN NGCF GCSAN GCEGNN GRU4Rec
+for model in Ours GCEGNN LESSR NISER GRU4Rec SASRec NGCF GCSAN BERT4Rec OurBERT4Rec TAGNN
 do
-   python /home/sh2/S3629862/projects/mine/RecBole-GNN/run_recbole_gnn.py -m $model -d yelp --config_files ./recbole_gnn/properties/overall.server.yaml --nproc -1
+   python /home/sh2/S3629862/projects/mine/RecBole-GNN/run_recbole_gnn.py -m $model -d diginetica5core --config_files ./recbole_gnn/properties/overall.server.yaml --nproc 1
 done
